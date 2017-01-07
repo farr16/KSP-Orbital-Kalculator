c00fb6436754291a22c9d9ce9656f4b04828b285
farr16 <msamfarr@outlook.com>
2017-01-06 20:43:50 -0800

    Adds functionality for populating the fragment container in the main activity, and for switching between fragments using the bottom bar navigation.

--- app/src/main/java/com/qwyxand/ksporbitalkalculator/MainActivity.java
+++ app/src/main/java/com/qwyxand/ksporbitalkalculator/MainActivity.java
@@ -1,13 +1,77 @@
 package com.qwyxand.ksporbitalkalculator;
 
+import android.support.annotation.IdRes;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.qwyxand.ksporbitalkalculator.viewfragments.*;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
+
+/**
+ * MainActivity
+ * Created by Matthew on 1/5/2017.
+ *
+ * Defines the main interface for the application.
+ * Handles user input on the bottom bar to manage switching between the fragment interfaces.
+ * Passes data between fragments and the orbital model.
+ */
+public class MainActivity extends AppCompatActivity implements OnTabSelectListener {
+
+    private BottomBar bottomBar;
+
+    private CalculatorFragment calculatorFragment;
+    private PhaseDisplayFragment phaseDisplayFragment;
+    private EjectDisplayFragment ejectDisplayFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            //CalculatorFragment calculatorFragment = new CalculatorFragment();
+            calculatorFragment = new CalculatorFragment();
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, calculatorFragment).commit();
+        }
+
+        bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(this);
+    }
+
+    public void onTabSelected(@IdRes int tabId) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (tabId) {
+            case R.id.tab_calculator:
+                if (calculatorFragment == null)
+                    calculatorFragment = new CalculatorFragment();
+                transaction.replace(R.id.fragment_container, calculatorFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+                break;
+            case R.id.tab_phase:
+                if (phaseDisplayFragment == null)
+                    phaseDisplayFragment = new PhaseDisplayFragment();
+                transaction.replace(R.id.fragment_container, phaseDisplayFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+                break;
+            case R.id.tab_eject:
+                if (ejectDisplayFragment == null)
+                    ejectDisplayFragment = new EjectDisplayFragment();
+                transaction.replace(R.id.fragment_container, ejectDisplayFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+                break;
+        }
     }
 }
