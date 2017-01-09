fb528f1809b8c35b20e59f33e487e6774a2c0a3f
farr16 <msamfarr@outlook.com>
2017-01-09 11:19:56 -0800

    Replaces TextView display on EjectDisplayFragment with a custom canvas (not implemented yet) which will display the orbital maneuver information visually.

--- app/src/main/java/com/qwyxand/ksporbitalkalculator/viewfragments/EjectDisplayFragment.java
+++ app/src/main/java/com/qwyxand/ksporbitalkalculator/viewfragments/EjectDisplayFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.qwyxand.ksporbitalkalculator.Body;
 import com.qwyxand.ksporbitalkalculator.MVC_Main;
@@ -24,30 +23,37 @@ import com.qwyxand.ksporbitalkalculator.R;
 public class EjectDisplayFragment extends Fragment implements MVC_Main.ViewOps ,
         MVC_Main.EjectViewOps {
 
-    private String text;
-    private TextView ejectTextView;
+    private Body origin = null;
+    private float ejectionAngle = Float.NEGATIVE_INFINITY;
+    private boolean inner = false;
+
+    private EjectDisplayCanvas ejectDisplayCanvas;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View ejectView = inflater.inflate(R.layout.fragment_eject_display, container, false);
-
-        if (text == null)
-            text = getString(R.string.eject_label);
-
-        ejectTextView = (TextView) ejectView.findViewById(R.id.EjectTestDisplay);
-        ejectTextView.setText(text);
+        ejectDisplayCanvas = (EjectDisplayCanvas) ejectView.findViewById(R.id.EjectionAngleDisplayCanvas);
+        ejectDisplayCanvas.setOrigin(origin);
+        ejectDisplayCanvas.setEjectionAngle(ejectionAngle);
+        ejectDisplayCanvas.setInner(inner);
 
         return ejectView;
     }
 
     @Override
     public void resetDisplay() {
-        text = getString(R.string.eject_label);
     }
 
     @Override
-    public void updateEjectDisplay(Body orig, float eject) {
-        text = "Origin: " + orig.name +  "\nEject: " + eject + "°";
+    public void updateEjectDisplay(Body orig, float eject, boolean in) {
+        origin = orig;
+        ejectionAngle = eject;
+        inner = in;
+        if (ejectDisplayCanvas != null) {
+            ejectDisplayCanvas.setOrigin(origin);
+            ejectDisplayCanvas.setEjectionAngle(ejectionAngle);
+            ejectDisplayCanvas.setInner(inner);
+        }
     }
 }
